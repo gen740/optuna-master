optuna/optuna

Modifying code examples to include training data

optuna/exceptions.py
@@ -13,21 +13,18 @@ class TrialPruned(OptunaError):
 
     Example:
 
-        .. testsetup::
-
-            import numpy as np
-            from sklearn.model_selection import train_test_split
-
-            np.random.seed(seed=0)
-            X = np.random.randn(200).reshape(-1, 1)
-            y = np.where(X[:, 0] < 0.5, 0, 1)
-            X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-            classes = np.unique(y)
-
         .. testcode::
 
             import optuna
+            import numpy as np
             from sklearn.linear_model import SGDClassifier
+
+            from sklearn.datasets import load_iris
+            from sklearn.model_selection import train_test_split
+
+            X, y = load_iris(return_X_y=True)
+            X_train, X_test, y_train, y_test = train_test_split(X, y)
+            classes = np.unique(y)
 
             def objective(trial):
                 alpha = trial.suggest_uniform('alpha', 0.0, 1.0)

optuna/pruners/median.py
@@ -11,21 +11,19 @@ class MedianPruner(PercentilePruner):
 
         We minimize an objective function with the median stopping rule.
 
-        .. testsetup::
-
-            import numpy as np
-            from sklearn.model_selection import train_test_split
-
-            np.random.seed(seed=0)
-            X = np.random.randn(200).reshape(-1, 1)
-            y = np.where(X[:, 0] < 0.5, 0, 1)
-            X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-            classes = np.unique(y)
-
         .. testcode::
 
             import optuna
+
+            import numpy as np
+
             from sklearn.linear_model import SGDClassifier
+            from sklearn.datasets import load_iris
+            from sklearn.model_selection import train_test_split
+
+            X, y = load_iris(return_X_y=True)
+            X_train, X_test, y_train, y_test = train_test_split(X, y)
+            classes = np.unique(y)
 
             def objective(trial):
                 alpha = trial.suggest_uniform('alpha', 0.0, 1.0)

optuna/pruners/nop.py
@@ -11,21 +11,19 @@ class NopPruner(BasePruner):
 
     Example:
 
-        .. testsetup::
-
-            import numpy as np
-            from sklearn.model_selection import train_test_split
-
-            np.random.seed(seed=0)
-            X = np.random.randn(200).reshape(-1, 1)
-            y = np.where(X[:, 0] < 0.5, 0, 1)
-            X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-            classes = np.unique(y)
-
         .. testcode::
 
             import optuna
+
+            import numpy as np
+
             from sklearn.linear_model import SGDClassifier
+            from sklearn.datasets import load_iris
+            from sklearn.model_selection import train_test_split
+
+            X, y = load_iris(return_X_y=True)
+            X_train, X_test, y_train, y_test = train_test_split(X, y)
+            classes = np.unique(y)
 
             def objective(trial):
                 alpha = trial.suggest_uniform('alpha', 0.0, 1.0)

optuna/pruners/percentile.py
@@ -74,21 +74,19 @@ class PercentilePruner(BasePruner):
 
     Example:
 
-        .. testsetup::
-
-            import numpy as np
-            from sklearn.model_selection import train_test_split
-
-            np.random.seed(seed=0)
-            X = np.random.randn(200).reshape(-1, 1)
-            y = np.where(X[:, 0] < 0.5, 0, 1)
-            X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-            classes = np.unique(y)
-
         .. testcode::
 
             import optuna
+
+            import numpy as np
+
             from sklearn.linear_model import SGDClassifier
+            from sklearn.datasets import load_iris
+            from sklearn.model_selection import train_test_split
+
+            X, y = load_iris(return_X_y=True)
+            X_train, X_test, y_train, y_test = train_test_split(X, y)
+            classes = np.unique(y)
 
             def objective(trial):
                 alpha = trial.suggest_uniform('alpha', 0.0, 1.0)

optuna/pruners/successive_halving.py
@@ -33,21 +33,19 @@ class SuccessiveHalvingPruner(BasePruner):
 
         We minimize an objective function with ``SuccessiveHalvingPruner``.
 
-        .. testsetup::
-
-            import numpy as np
-            from sklearn.model_selection import train_test_split
-
-            np.random.seed(seed=0)
-            X = np.random.randn(200).reshape(-1, 1)
-            y = np.where(X[:, 0] < 0.5, 0, 1)
-            X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-            classes = np.unique(y)
-
         .. testcode::
 
             import optuna
+
+            import numpy as np
+
             from sklearn.linear_model import SGDClassifier
+            from sklearn.datasets import load_iris
+            from sklearn.model_selection import train_test_split
+
+            X, y = load_iris(return_X_y=True)
+            X_train, X_test, y_train, y_test = train_test_split(X, y)
+            classes = np.unique(y)
 
             def objective(trial):
                 alpha = trial.suggest_uniform('alpha', 0.0, 1.0)

gen740/optuna-master

optuna/exceptions.py
@@ -13,21 +13,18 @@ class TrialPruned(OptunaError):
 
     Example:
 
-        .. testsetup::
-
-            import numpy as np
-            from sklearn.model_selection import train_test_split
-
-            np.random.seed(seed=0)
-            X = np.random.randn(200).reshape(-1, 1)
-            y = np.where(X[:, 0] < 0.5, 0, 1)
-            X_train, X_valid, y_train, y_valid = train_test_split(X, y, random_state=0)
-            classes = np.unique(y)
-
         .. testcode::
 
-            import optuna
+            import numpy as np
+            from sklearn.datasets import load_iris
             from sklearn.linear_model import SGDClassifier
+            from sklearn.model_selection import train_test_split
+
+            import optuna
+
+            X, y = load_iris(return_X_y=True)
+            X_train, X_valid, y_train, y_valid = train_test_split(X, y)
+            classes = np.unique(y)
 
             def objective(trial):
                 alpha = trial.suggest_uniform('alpha', 0.0, 1.0)

optuna/pruners/median.py
@@ -11,21 +11,18 @@ class MedianPruner(PercentilePruner):
 
         We minimize an objective function with the median stopping rule.
 
-        .. testsetup::
-
-            import numpy as np
-            from sklearn.model_selection import train_test_split
-
-            np.random.seed(seed=0)
-            X = np.random.randn(200).reshape(-1, 1)
-            y = np.where(X[:, 0] < 0.5, 0, 1)
-            X_train, X_valid, y_train, y_valid = train_test_split(X, y, random_state=0)
-            classes = np.unique(y)
-
         .. testcode::
 
-            import optuna
+            import numpy as np
+            from sklearn.datasets import load_iris
             from sklearn.linear_model import SGDClassifier
+            from sklearn.model_selection import train_test_split
+
+            import optuna
+
+            X, y = load_iris(return_X_y=True)
+            X_train, X_valid, y_train, y_valid = train_test_split(X, y)
+            classes = np.unique(y)
 
             def objective(trial):
                 alpha = trial.suggest_uniform('alpha', 0.0, 1.0)

optuna/pruners/nop.py
@@ -11,21 +11,18 @@ class NopPruner(BasePruner):
 
     Example:
 
-        .. testsetup::
-
-            import numpy as np
-            from sklearn.model_selection import train_test_split
-
-            np.random.seed(seed=0)
-            X = np.random.randn(200).reshape(-1, 1)
-            y = np.where(X[:, 0] < 0.5, 0, 1)
-            X_train, X_valid, y_train, y_valid = train_test_split(X, y, random_state=0)
-            classes = np.unique(y)
-
         .. testcode::
 
-            import optuna
+            import numpy as np
+            from sklearn.datasets import load_iris
             from sklearn.linear_model import SGDClassifier
+            from sklearn.model_selection import train_test_split
+
+            import optuna
+
+            X, y = load_iris(return_X_y=True)
+            X_train, X_valid, y_train, y_valid = train_test_split(X, y)
+            classes = np.unique(y)
 
             def objective(trial):
                 alpha = trial.suggest_uniform('alpha', 0.0, 1.0)

optuna/pruners/percentile.py
@@ -76,21 +76,18 @@ class PercentilePruner(BasePruner):
 
     Example:
 
-        .. testsetup::
-
-            import numpy as np
-            from sklearn.model_selection import train_test_split
-
-            np.random.seed(seed=0)
-            X = np.random.randn(200).reshape(-1, 1)
-            y = np.where(X[:, 0] < 0.5, 0, 1)
-            X_train, X_valid, y_train, y_valid = train_test_split(X, y, random_state=0)
-            classes = np.unique(y)
-
         .. testcode::
 
-            import optuna
+            import numpy as np
+            from sklearn.datasets import load_iris
             from sklearn.linear_model import SGDClassifier
+            from sklearn.model_selection import train_test_split
+
+            import optuna
+
+            X, y = load_iris(return_X_y=True)
+            X_train, X_valid, y_train, y_valid = train_test_split(X, y)
+            classes = np.unique(y)
 
             def objective(trial):
                 alpha = trial.suggest_uniform('alpha', 0.0, 1.0)

optuna/pruners/successive_halving.py
@@ -33,21 +33,18 @@ class SuccessiveHalvingPruner(BasePruner):
 
         We minimize an objective function with ``SuccessiveHalvingPruner``.
 
-        .. testsetup::
-
-            import numpy as np
-            from sklearn.model_selection import train_test_split
-
-            np.random.seed(seed=0)
-            X = np.random.randn(200).reshape(-1, 1)
-            y = np.where(X[:, 0] < 0.5, 0, 1)
-            X_train, X_valid, y_train, y_valid = train_test_split(X, y, random_state=0)
-            classes = np.unique(y)
-
         .. testcode::
 
-            import optuna
+            import numpy as np
+            from sklearn.datasets import load_iris
             from sklearn.linear_model import SGDClassifier
+            from sklearn.model_selection import train_test_split
+
+            import optuna
+
+            X, y = load_iris(return_X_y=True)
+            X_train, X_valid, y_train, y_valid = train_test_split(X, y)
+            classes = np.unique(y)
 
             def objective(trial):
                 alpha = trial.suggest_uniform('alpha', 0.0, 1.0)